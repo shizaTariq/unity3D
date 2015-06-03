5de5894b53fef8ac79ea3081148bd03364ee5dba
shizaTariq <shizatariq@outlook.com>
2015-06-03 09:19:38 -0700

    commit message

---
+++ Assets/cameraScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Sphere");
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/sphereScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphereScript : MonoBehaviour {
+	Rigidbody rigidBody;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+
+		rigidBody = GetComponent<Rigidbody> ();
+		speed = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidBody.AddForce (movement * speed);
+	}
+
+}
